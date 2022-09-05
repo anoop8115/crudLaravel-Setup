3e1b75bc82709d6fa356ad6b6a4bd32129333d06
anoop8115 <rachitanoop@gmail.com>
2022-09-05 12:36:57 +0530

    first setup

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::resource('meter_datas', App\Http\Controllers\API\MeterDataAPIController::class);
-
-Route::get('/add',[App\Http\Controllers\API\MeterDataAPIController::class,'api_store']);
-Route::get('/add_system_info',[App\Http\Controllers\API\MeterDataAPIController::class,'add_system_info']);
-Route::get('/add_system_alarm',[App\Http\Controllers\API\MeterDataAPIController::class,'add_system_alarm']);
-Route::get('/rodata',[App\Http\Controllers\API\MeterDataAPIController::class,'get_ro']);
